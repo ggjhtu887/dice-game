db638fc9a878b9df2918b0216bf596c7bf2c18c2
Akhilesh <akhiljadi41@gmail.com>
2024-07-17 15:01:10 +0530

    first commit

---
+++ src/components/RoleDice.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+import styled from 'styled-components';
+
+const RoleDice = ({roleDice,currentDice}) => {
+  
+  return (
+    <DiceContainer>
+      <div className='dice' onClick= {roleDice}>
+        <img src={`/dice/dice_${currentDice}.png`} alt="" />
+      </div>
+      <p>Click on Dice to roll</p>
+    </DiceContainer>
+  )
+}
+
+export default RoleDice
+
+const DiceContainer = styled.div`
+  margin-top: 48px;
+  display:flex;
+  flex-direction:column;
+  align-items:center;
+
+  .dice{
+    cursor: pointer;
+  }
+
+  p{
+    font-size: 24px;
+  }
+`
---
+++ src/components/StartGame.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components'
+import { Button } from '../styled/Button';
+
+export default function StartGame({toggle}) {
+  return (
+    <Container>
+
+     <div>
+       <img src="/images/dices.png" alt="" />
+     </div>
+
+      <div className='content'>
+        <h1>Dice Game</h1>
+        <Button onClick={toggle}>Play now</Button>
+      </div>
+
+    </Container>
+  )
+}
+
+const Container = styled.div`
+   max-width: 1180px;
+   height:100vh;
+   display:flex;
+   margin: 0 auto;
+   align-items:center;
+
+   .content{
+    h1{
+      font-size:96px;
+      white-space:nowrap;
+    }
+   }
+`;
+
